12a7712738fe15bde108423f7d27f607ae3b71c4
Say.li <120011676@qq.com>
2026-06-30 09:23:44 +0800

    '变maven'

---
+++ src/main/java/com/github/qq120011676/tater/App.java
@@ -0,0 +1,13 @@
+package com.github.qq120011676.tater;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
